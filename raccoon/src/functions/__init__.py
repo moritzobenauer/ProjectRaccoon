13a136e2857b74edc32b2e65b3ae355a31158d18
kaisszs <n.spauszus@gmail.com>
2023-12-24 09:01:32 +0100

    rename advanced functions->util

--- raccoon/src/functions/__init__.py
+++ raccoon/src/functions/__init__.py
@@ -1,3 +1,2 @@
 from standard import generate_sequence
-
-from advanced import PDBtoXYZ, CheckPDB, Visualize, Distances, CheckMinimalDistance
+from util import PDBtoXYZ, CheckPDB, Visualize, Distances, CheckMinimalDistance
