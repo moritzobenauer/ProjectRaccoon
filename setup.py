b9e48f5f0f3cfcfeb92b7face5450bfd8c322cf1
Kai N. Spauszus <n.spauszus@gmail.com>
2024-01-08 21:03:06 +0100

    add long description in setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,24 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
+long_description = Path(Path(__file__).parent / "README.md").read_text()
+requirements = Path(Path(__file__).parent / "requirements.txt").read_text().splitlines()
+
 setup(
     name="raccoon",
     version="1.0.0",
     description="Project RACCOON is a Python tool designed for the generation of PDB (Protein Data Bank) files for polymer peptide conjugates, polypeptides, and polymers in a building block fashion.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Moritz L. Obenauer, Kai N. Spauszus.",
     url="https://github.com/moritzobenauer/ProjectRaccoon",
     packages=find_packages(),
-    package_data={"raccoon": ["src/data/*.json"]},
+    package_data={
+        "raccoon": ["src/data/*.json", "requirements.txt", "README.md", "LICENSE"]
+    },
     test_suite="raccoon/tests",
     entry_points={"console_scripts": ["raccoon=raccoon.__main__:main"]},
-    install_requires=[
-        "biopandas==0.4.1",
-        "numpy==1.26.3",
-        "pandas==2.1.4",
-        "py3Dmol==2.0.4",
-        "questionary==2.0.1",
-        "rich==13.7.0",
-        "scipy==1.11.4",
-        "setuptools==68.2.2",
-        "tqdm==4.66.1",
-    ],
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License (GPL)",
