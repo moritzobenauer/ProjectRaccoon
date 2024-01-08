18d1e490eb0e9b4ebd0b07eccc7133949ab51aa0
Kai N. Spauszus <n.spauszus@gmail.com>
2024-01-08 20:43:03 +0100

    change description in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,7 @@ from pathlib import Path
 setup(
     name="raccoon",
     version="1.0.0",
-    description="Project RACCOON\
-                 (Rapid Automated Construction of Conjugates using the Optimized OPLS Input)\
-                 is a Python tool designed for the generation of PDB (Protein Data Bank) files\
-                 for polymer peptide conjugates, polypeptides, and polymers in a building block fashion.\
-                 It allows for the easy addition of new monomers, incorporation of polypeptide and\
-                 polymer sequences in text form, and outputs PDB and XYZ files.",
+    description="Project RACCOON is a Python tool designed for the generation of PDB (Protein Data Bank) files for polymer peptide conjugates, polypeptides, and polymers in a building block fashion.",
     author="Moritz L. Obenauer, Kai N. Spauszus.",
     url="https://github.com/moritzobenauer/ProjectRaccoon",
     packages=find_packages(),
