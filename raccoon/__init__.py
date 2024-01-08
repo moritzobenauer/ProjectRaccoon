f078af1aa431f830084f42da23dd6f30376c4822
Kai N. Spauszus <n.spauszus@gmail.com>
2024-01-08 08:17:33 +0100

    export all relevant functions

--- raccoon/__init__.py
+++ raccoon/__init__.py
@@ -1,3 +1,11 @@
-from .src.data import Monomer, Monomers
-from .src.functions import generate_file, generate_sequence
+from .src.data import Monomer, Monomers, Sequence, Atom
+from .src.functions import (
+    generate_file,
+    generate_sequence,
+    Visualize,
+    get_elements_and_coords_from_pdb,
+    get_links_from_pdb,
+    pdb_to_xyz,
+    calc_minimal_distance,
+)
 from .src.ui import start_racoon, welcome, tschau_kakao
