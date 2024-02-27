14e454c5a049e13843039ad5aba18900dc7fc8f1
Kai N. Spauszus <n.spauszus@gmail.com>
2024-02-27 09:13:41 +0100

    running test from tests/__main__.py

--- project_raccoon/tests/__main__.py
+++ project_raccoon/tests/__main__.py
@@ -1,4 +1,11 @@
-from unittest import main
+import unittest
+from pathlib import Path
 
 if __name__ == "__main__":
-    main()
+
+    root_dir = Path(__file__).resolve().parents[2]
+
+    loader = unittest.TestLoader()
+    testSuite = loader.discover(start_dir=root_dir, pattern="test_*.py")
+    runner = unittest.TextTestRunner()
+    runner.run(testSuite)
